a3ee862dbec214e3b403bd42c0c281dfcdf5ed71
wjscksgk0601 <jch06060106@gmail.com>
2023-08-02 14:09:46 +0900

    canvas 마무리

--- js/common.js
+++ js/common.js
@@ -1,14 +1,16 @@
+// 변수들
+const container = document.querySelector('.container');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-let isDrag = false;
 let bottom = document.querySelector('.bottom');
-let select = null;
+let isDrag = false;
 let i = 0;
 
+// 유닛들
 const obj = {
   육 : {
-      육군 : 'soldier',
-      탱크 : 'tank', 
+    육군 : 'soldier',
+    탱크 : 'tank', 
   },
   해 : {
     잠수함 : ''
@@ -18,18 +20,13 @@ const obj = {
   }
 }
 
-document.querySelector('.ground').addEventListener('click', () => {
-  addBottom(obj.육);
-});
+// canvas 초기 설정
+canvas.width=1200;
+canvas.height=500;
+ctx.beginPath();
+ctx.stroke();
 
-document.querySelector('.sea').addEventListener('click', () => {
-  addBottom(obj.해);
-});
-
-document.querySelector('.sky').addEventListener('click', () => {
-  addBottom(obj.공);
-});
-
+// 여기서부터 함수들
 function addBottom(e) {
   bottom.textContent = '';
   Object.values(e).forEach(el => {
@@ -43,33 +40,17 @@ function addBottom(e) {
     createImg.setAttribute('ondragstart', 'drag(event)');
     createDiv.classList.add('box');
     createDiv.setAttribute("ondragover", "dragEnter(event)");
-    createDiv.addEventListener('dragstart', () => { select = el });
     createDiv.append(createImg);
     bottom.append(createDiv);
   });
 }
 
-document.querySelector('.remove').addEventListener('click', e => { 
-  let img = document.querySelectorAll('.img');
-  select = 'remove';
-  img.forEach(el => {
-    el.addEventListener('click', (ee) => removeObject(ee))
-  });
-});
-
-document.querySelector('.removeAll').addEventListener('click', () => {
-  select = 'removeAll';
-  document.querySelector('.container').textContent = '';
-  i = 0;
-});
-  
 function dragEnter(ev) {
   ev.preventDefault();  
 }
 
 function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
-
 }
 
 function drop(ev) {
@@ -84,51 +65,73 @@ function drop(ev) {
 }
 
 function removeObject(ee) {
-  if(select == 'remove' && !ee.target.classList.contains('cantRemove')) {
+  if(!ee.target.classList.contains('cantRemove')) {
     ee.target.remove();
     i-=1;
     if(i <= 0) i = 0;
   }
 }
 
+function draw(e) {
+  isDrag = true;
+  ctx.moveTo(e.offsetX,e.offsetY);
+}
 
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+}
 
+// 여기서부터 이벤트리스너들
+document.querySelector('.ground').addEventListener('click', () => addBottom(obj.육) );
+document.querySelector('.sea').addEventListener('click', () => addBottom(obj.해) );
+document.querySelector('.sky').addEventListener('click', () => addBottom(obj.공) );
 
-
-canvas.width=1200;
-canvas.height=500;
-
-ctx.beginPath();
-ctx.stroke();
-
-document.querySelector(".draw").addEventListener("click", () => {
-    canvas.classList.toggle("draww");
-    if(canvas.classList.contains("draww")) {
-        canvas.addEventListener('mouseover', (e) => {
-            e.target.style.cursor = "crosshair";
-        });
-        canvas.addEventListener('mousedown', draw);
-        canvas.addEventListener('mousemove', e => {
-            if(!isDrag) return;
-            ctx.lineTo(e.offsetX,e.offsetY);
-            ctx.stroke();
-        });
-        canvas.addEventListener('mouseup', () => {
-            isDrag = false;
-        });
-        canvas.addEventListener('mouseenter', () => {
-            ctx.beginPath();
-        });
-    }
-    else {
-        canvas.removeEventListener('mousedown', draw);
-        canvas.addEventListener('mouseover', (e) => {
-            e.target.style.cursor = "default";
-        });
-    }
+// 유닛 단일 제거
+document.querySelector('.remove').addEventListener('click', e => { 
+  let img = document.querySelectorAll('.img');
+  img.forEach(el => {
+    el.addEventListener('click', (ee) => removeObject(ee))
+  });
 });
 
-function draw(e) {
-    isDrag = true;
-    ctx.moveTo(e.offsetX,e.offsetY);
-}
+// 유닛 전체 제거
+document.querySelector('.removeAll').addEventListener('click', () => {
+  Array.from(container.children).forEach(element => {
+    if(element.classList.contains('img')) {
+      element.remove();
+      i = 0;
+    }
+  });
+});
+
+// 캔버스 그림판코드
+document.querySelector(".draw").addEventListener("click", () => {
+  canvas.classList.toggle("draww");
+  if(canvas.classList.contains("draww")) {
+    canvas.addEventListener('mouseover', (e) => {
+      e.target.style.cursor = "crosshair";
+    });
+    canvas.addEventListener('mousedown', draw);
+    canvas.addEventListener('mousemove', e => {
+      if(!isDrag) return;
+      ctx.lineTo(e.offsetX,e.offsetY);
+      ctx.stroke();
+    });
+    canvas.addEventListener('mouseup', () => {
+      isDrag = false;
+    });
+    canvas.addEventListener('mouseenter', () => {
+      ctx.beginPath();
+    });
+  }
+  else {
+    canvas.removeEventListener('mousedown', draw);
+    canvas.addEventListener('mouseover', (e) => {
+      e.target.style.cursor = "default";
+    });
+  }
+});
+
+// 캔버스 지우기 이벤트 리스너
+document.querySelector('.canvasClear').addEventListener('click', clearCanvas);
